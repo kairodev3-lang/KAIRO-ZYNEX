d3783a2260d6ecb15bc0742c38145c11fccd7157
kairodev3-lang <kairodev3@gmail.com>
2026-08-28 15:58:08 -0400

    bot

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '50937107029',
+    botName: '𝙳𝚁𝚄𝚉𝚉 𝚇-𝙼𝙳',
+    ownerName: '𝙳𝚁𝚄𝚉𝚉'
+};
